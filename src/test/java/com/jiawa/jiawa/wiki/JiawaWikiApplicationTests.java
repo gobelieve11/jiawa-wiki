a85b33f71bcdaeb1035eeb0e49e80ce0235b8cf3
923967798 <923967798@qq.com>
2021-04-09 16:37:29 +0800

    2-2

---
+++ src/test/java/com/jiawa/jiawa/wiki/JiawaWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.jiawa.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiawaWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
