a0a3fff50a9a563e23f3d9b081e40c75454181e7
923967798 <923967798@qq.com>
2021-04-09 22:00:09 +0800

    修改启动信息

--- src/main/java/com/jiawa/jiawa/wiki/JiawaWikiApplication.java
+++ src/main/java/com/jiawa/jiawa/wiki/JiawaWikiApplication.java
@@ -1,13 +1,25 @@
 package com.jiawa.jiawa.wiki;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class JiawaWikiApplication {
+	private final static Logger logger = LoggerFactory.getLogger(JiawaWikiApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(JiawaWikiApplication.class, args);
+		SpringApplication app = new SpringApplication(JiawaWikiApplication.class);
+		Environment env =app.run(args).getEnvironment();
+		logger.info("启动成功！");
+		logger.info("地址为\thttp://127.0.01:{}",env.getProperty("server.port"));
+
+
 	}
 
 }
