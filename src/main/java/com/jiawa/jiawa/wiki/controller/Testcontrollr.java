8057381237dd935cb1bc68b98eb5ace5e282bef9
923967798 <923967798@qq.com>
2021-04-09 22:57:55 +0800

    http client 测试接口

--- src/main/java/com/jiawa/jiawa/wiki/controller/Testcontrollr.java
+++ src/main/java/com/jiawa/jiawa/wiki/controller/Testcontrollr.java
@@ -1,14 +1,22 @@
 package com.jiawa.jiawa.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class Testcontrollr {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return ("Hello World!");
+    }
+
+    @PostMapping("/hello/post")
+    public String hello_post(String name){
+        return ("Hello World! Post" +name);
     }
 
 
