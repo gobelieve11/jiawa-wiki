aab4598263047d68ec6f6fedd2621ecba72e066c
923967798 <923967798@qq.com>
2021-04-09 16:35:52 +0800

    2-2

---
+++ src/main/java/com/jiawa/jiawa/wiki/JiawaWikiApplication.java
@@ -0,0 +1,13 @@
+package com.jiawa.jiawa.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiawaWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiawaWikiApplication.class, args);
+	}
+
+}
