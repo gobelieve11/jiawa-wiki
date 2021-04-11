157df8ca318c60653568f60f41e819e2fdb2f317
923967798 <923967798@qq.com>
2021-04-12 00:33:08 +0800

    copyutil工具类

--- src/main/java/com/jiawa/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/jiawa/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.jiawa.jiawa.wiki.domain.EbookExample;
 import com.jiawa.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.jiawa.wiki.req.EbookReq;
 import com.jiawa.jiawa.wiki.resp.EbookResp;
+import com.jiawa.jiawa.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -26,15 +27,17 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList =ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList =new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp resp = new EbookResp();
-            BeanUtils.copyProperties(ebook,resp);
-            respList.add(resp);
-            
-        }
+//        List<EbookResp> respList =new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
 
-        return respList;
+//  //  EbookResp resp = new EbookResp();
+// //   BeanUtils.copyProperties(ebook,resp);
+//                             //对象复制
+//            EbookResp resp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(resp);
+//
+//        }
+        return CopyUtil.copyList(ebookList, EbookResp.class);
 
 
 
